e141facd0baca8513150fab7f58c0596d9d1b79f
rohanwagh009 <69116445+rohanwagh009@users.noreply.github.com>
2025-10-05 23:19:09 +0530

    navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,31 +6,35 @@ import { useAuthStore } from "@/store/Auth";
 import slugify from "@/utils/slugify";
 
 export default function Header() {
-    const { user } = useAuthStore();
+  const { user } = useAuthStore();
 
-    const navItems = [
-        {
-            name: "Home",
-            link: "/",
-            icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
-        },
-        {
-            name: "Questions",
-            link: "/questions",
-            icon: <IconWorldQuestion className="h-4 w-4 text-neutral-500 dark:text-white" />,
-        },
-    ];
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Questions",
+      link: "/questions",
+      icon: (
+        <IconWorldQuestion className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
 
-    if (user)
-        navItems.push({
-            name: "Profile",
-            link: `/users/${user.$id}/${slugify(user.name)}`,
-            icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
-        });
+  if (user)
+    navItems.push({
+      name: "Profile",
+      link: `/users/${user.$id}/${slugify(user.name)}`,
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    });
 
-    return (
-        <div className="relative w-full">
-            <FloatingNav navItems={navItems} />
-        </div>
-    );
+  return (
+    <div className="relative w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
 }
